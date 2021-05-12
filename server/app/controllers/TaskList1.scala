306bc0fa02fd20f96ffefe6c98dec88d89630fa2
azdrogov <azdrogov@naumen.ru>
2021-05-12 08:46:28 +0500

    Making model

--- server/app/controllers/TaskList1.scala
+++ server/app/controllers/TaskList1.scala
@@ -1,14 +1,17 @@
 package controllers
 
-import play.api.mvc.{BaseController, ControllerComponents}
+import models.TaskListInMemoryModel
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 
 import javax.inject.{Inject, Singleton}
 
 @Singleton
 class TaskList1 @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  def taskList() = Action {
-    val tasks = List("task1", "task2", "task3")
+  def taskList(): Action[AnyContent] = Action {
+    val username = "Andrew"
+
+    val tasks = TaskListInMemoryModel.getTasks(username)
     Ok(views.html.taskList1(tasks))
   }
 
@@ -21,12 +24,29 @@ class TaskList1 @Inject()(val controllerComponents: ControllerComponents) extend
     postVals.map { args =>
       val username = args("username").head
       val password = args("password").head
-      Redirect(routes.TaskList1.taskList())
-    }.getOrElse(Redirect(routes.TaskList1.login1))
+      if (TaskListInMemoryModel.validate(username, password)) {
+        Redirect(routes.TaskList1.taskList())
+      } else {
+        Redirect(routes.TaskList1.login)
+      }
+    }.getOrElse(Redirect(routes.TaskList1.login))
   }
 
-  def login1 = Action {
-    Ok(views.html.login1())
+  def createUser = Action { request =>
+    val postVals = request.body.asFormUrlEncoded
+    postVals.map { args =>
+      val username = args("username").head
+      val password = args("password").head
+      if (TaskListInMemoryModel.createUser(username, password)) {
+        Redirect(routes.TaskList1.taskList())
+      } else {
+        Redirect(routes.TaskList1.login)
+      }
+    }.getOrElse(Redirect(routes.TaskList1.login))
+  }
+
+  def login = Action {
+    Ok(views.html.login())
   }
 
 }
---
+++ server/app/models/TaskListInMemoryModel.scala
@@ -0,0 +1,30 @@
+package models
+
+import collection.mutable
+
+object TaskListInMemoryModel {
+  private val users = mutable.Map[String, String]("Andrew" -> "pass")
+  private val tasks = mutable.Map[String, List[String]]("Andrew" -> List("Eat", "Work", "Sing"))
+
+  def validate(username: String, password: String): Boolean = {
+    users.get(username).contains(password)
+  }
+
+  def createUser(username: String, password: String): Boolean = {
+    if (users.contains(username)) false
+    else {
+      users(username) = password
+      true
+    }
+  }
+
+  def getTasks(username: String): Seq[String] = {
+    tasks.getOrElse(username, Nil)
+  }
+
+  def addTask(username: String, task: String): Unit = ???
+
+  def removeTask(username: String, index: Int): Boolean = ???
+
+
+}
